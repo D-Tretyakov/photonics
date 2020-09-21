4d0cce27e3b0cdf533df4290bd6dabc945ae898a
Daniil <tretyakov.daniil.edcuation@gmail.com>
2020-09-21 15:24:12 +0300

    reqs + small fix in test.py

--- test.py
+++ test.py
@@ -14,13 +14,16 @@ def main():
 
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
-            # print(event.__dict__)
+            # print(event.obj.__dict__)
             # {'date': 1596138570, 'from_id': 212285182, 'id': 997, 'out': 0, 'peer_id': 212285182, 'text': 'dfg', 'conversation_message_id': 65, 'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [], 'is_hidden': False}
             user_id = event.obj.message['from_id']
             text = event.obj.message['text']
             # page = vk.users.get(user_id=116584678)
 
             if user_id == 116584678 and random.random() > 0.3:
-                vk.messages.send(user_id=usr_id, message='Ты пидор', random_id=0)
+                vk.messages.send(user_id=user_id, message='Ты пидор', random_id=0)
 
-            vk.messages.send(user_id=usr_id, message=text.upper(), random_id=0)
+            vk.messages.send(user_id=user_id, message=text.upper(), random_id=0)
+
+if __name__ == "__main__":
+    main()
