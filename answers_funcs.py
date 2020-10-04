5fc963e1e622ed8a3e9ee38eda3ed83e8bf329cf
Марин Денис <55432164+MarksmanM@users.noreply.github.com>
2020-10-04 15:38:44 +0300

    fix))0)

--- answers_funcs.py
+++ answers_funcs.py
@@ -6,5 +6,5 @@
 def ans_func(tasks):
     answer = "ЗАДАНИЯ ПО ПРЕДМЕТУ:"+"\n"
     for task in tasks:
-        answer += "Домашка " + tasks.index(task) + ":\t" + task + "\n"
+        answer += "Домашка " + str(tasks.index(task)) + ":\t" + task + "\n"
     return answer
