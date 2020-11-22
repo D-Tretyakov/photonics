fcbf919419c3625819bf86fee86be26e82f265c2
Rusik20 <ruslanfiztech@gmail.com>
2020-11-22 23:40:12 +0300

    Added code

--- какаха.py
+++ какаха.py
@@ -4,18 +4,17 @@ import re
 
 def convert_date(str_date):
     # дата должна приходить в формате дд.мм.гггг (10.11.2020)
-    result = re.search(r'\d{2}\.\d{2}\.\d{4}', str_date)
+    result = re.search(r'\d{2}.\d{2}.\d{4}', str_date)
     if result is not None:
         return datetime.datetime.strptime(str_date, '%d.%m.%Y')
     else:
-        return None
+        return 'None'
 
 
 def convert_urls(urls_str):
     pattern = r'\bhttps://\S+\b'  # url каждого файла должен идти через пробелы
     result = re.findall(pattern, urls_str)
     if result is not None:
-        print(result[0])
         return result
     else:
         return 'None'
@@ -48,35 +47,26 @@ def parse_message(message):
 
 
     elif message.startswith('!добавить'):
-        # !добавить дз по subj на from_date до to_date task_text файл:[url1 url2 url3] <- структура команды
-        if 'файл' in message:
-            pattern = r'!добавить\sдз\sпо\s(\D*)\sна\s(\S*)\sдо\s(\S*)\s(\D*)\sфайл:(\D*)'
-            result = re.search(pattern, message)
-            if not result:
-                return {'error': 'invalid command format'}
-            args = result.groups()
-            subj = args[0]
-            for_date_str = args[1]
-            to_date_str = args[2]
-            task_text = args[3]
-            urls_str = args[4]
-            urls = convert_urls(urls_str)
-        else:
-            pattern = r'!добавить\sдз\sпо\s(\D*)\sна\s(\S*)\sдо\s(\S*)\s(\D*)'
-            result = re.search(pattern, message)
-            if not result:
-                return {'error': 'invalid command format'}
-            args = result.groups()
-            subj = args[0]
-            for_date_str = args[1]
-            to_date_str = args[2]
-            task_text = args[3]
-            urls = 'None'
+        # !добавить дз по subj на from_date до to_date "task_text" [url1 url2 url3] <- структура команды
+        pattern = r'!добавить\sдз\sпо\s(\D*)\sна\s(\S*)\sдо\s(\S*)\s(\"(.*?)\")\s(\[(.*?)\])'
+        result = re.search(pattern, message)
+
+        if not result:
+            return {'error': 'invalid command format'}
+
+        args = result.groups()
+        subj = args[0]
+        for_date_str = args[1]
+        to_date_str = args[2]
+        task_text = args[3]
+        urls_str = args[4]
 
         for_date = convert_date(for_date_str)
         to_date = convert_date(to_date_str)
         if for_date is None or to_date is None:
             return {'error': 'invalid date'}
+
+        urls = convert_urls(urls_str)
 
         return {'command': 'добавить',
                 'subj': subj,
@@ -94,5 +84,7 @@ def parse_message(message):
         subj = args[0]
         for_date = args[1]
         return {'command': 'удалить', 'subj': subj, 'for_date': for_date}
-    else:
-        return {'error': 'invalid command format'}
+
+
+str1 = str(input())
+print(parse_message(str1))
