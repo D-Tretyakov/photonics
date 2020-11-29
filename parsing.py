3083fce4d6605bd0acb3eb59642f50a1f233bc80
Rusik20 <ruslanfiztech@gmail.com>
2020-11-29 22:34:27 +0300

    the end

--- parsing.py
+++ parsing.py
@@ -12,13 +12,15 @@ def convert_date(str_date):
 
 
 def convert_urls(urls_str):
-    pattern = r'\bhttps://\S+\b'  # url каждого файла должен идти через пробелы
+    pattern = r'\bhttps://\S+\b'  #  url каждого файла должен идти через пробелы
     result = re.findall(pattern, urls_str)
+
     if result is not None:
-        print(result[0])
+
         return result
     else:
-        return 'None'
+
+        return None
 
 
 def parse_message(message):
@@ -26,52 +28,53 @@ def parse_message(message):
         return {'error': 'invalid command format'}
 
     if message.startswith('!показать'):
-        if 'на' in message:
-            pattern = r'!показать\sдз\sпо\s(\D*)\sна\s(\S*)'  # шаблон для команды
-            result = re.search(pattern, message)  # возвращаем список из
+        if 'на' and 'количество' in message:
+            pattern = r'!показать\sдз\sпо\s(\D*)\sна\s(\S*)\sколичество(.*)'  # шаблон для команды Показать на конкретную дату и количество
+            result = re.search(pattern, message)
             if not result:
                 return {'error': 'invalid command format'}
             args = result.groups()
             subj = args[0]
-            to_date = args[1]
-            return {'command': 'показать', 'subj': subj, 'to_date': to_date, 'amount': '1'}
+            to_date_str = args[1]
+            amount = int(args[2])
+            to_date = convert_date(to_date_str)
+            return {'command': 'показать', 'subj': subj, 'to_date': to_date, 'amount': amount}
+
+        elif 'на' in message:
+            pattern = r'!показать\sдз\sпо\s(\D*)\sна\s(\S*)'  # шаблон для команды Показать на конкретную дату
+            result = re.search(pattern, message)
+            if not result:
+                return {'error': 'invalid command format'}
+            args = result.groups()
+            subj = args[0]
+            to_date_str = args[1]
+            to_date = convert_date(to_date_str)
+            return {'command': 'показать', 'subj': subj, 'to_date': to_date, 'amount': 1}
 
         else:
-            pattern = r'!показать\sдз\sпо\s(\D*)'
-            result = re.search(pattern, message)  # возвращаем список из
+            pattern = r'!показать\sдз\sпо\s(\D*)'   # Шаблон для команды
+            result = re.search(pattern, message)
             if not result:
                 return {'error': 'invalid command format'}
             args = result.groups()
             subj = args[0]
-            return {'command': 'показать', 'subj': subj, 'amount': '5'}
+            return {'command': 'показать', 'subj': subj, 'amount': None}
 
 
 
     elif message.startswith('!добавить'):
-        # !добавить дз по subj на from_date до to_date task_text файл:[url1 url2 url3] <- структура команды
-        if 'файл' in message:
-            pattern = r'!добавить\sдз\sпо\s(\D*)\sна\s(\S*)\sдо\s(\S*)\s(\D*)\sфайл:(\D*)'
-            result = re.search(pattern, message)
-            if not result:
-                return {'error': 'invalid command format'}
-            args = result.groups()
-            subj = args[0]
-            for_date_str = args[1]
-            to_date_str = args[2]
-            task_text = args[3]
-            urls_str = args[4]
-            urls = convert_urls(urls_str)
-        else:
-            pattern = r'!добавить\sдз\sпо\s(\D*)\sна\s(\S*)\sдо\s(\S*)\s(\D*)'
-            result = re.search(pattern, message)
-            if not result:
-                return {'error': 'invalid command format'}
-            args = result.groups()
-            subj = args[0]
-            for_date_str = args[1]
-            to_date_str = args[2]
-            task_text = args[3]
-            urls = 'None'
+        # !добавить дз по subj на from_date до to_date "task_text" [url1 url2 url3] <- структура команды
+        pattern = r'!добавить\sдз\sпо\s(\D*)\sза\s(\S*)\sдо\s(\S*)\s(\".*?\")(.*)' # Шаблон для команды добавить , обратить внимание на кавычки !!!
+        result = re.search(pattern, message)
+        if not result:
+            return {'error': 'invalid command format'}
+        args = result.groups()
+        subj = args[0]
+        for_date_str = args[1]
+        to_date_str = args[2]
+        task_text = args[3]
+        urls_str = args[4]
+        urls = convert_urls(urls_str)
 
         for_date = convert_date(for_date_str)
         to_date = convert_date(to_date_str)
@@ -86,13 +89,14 @@ def parse_message(message):
                 'urls': urls}
 
     elif message.startswith('!удалить'):
-        pattern = r'!удалить\sдз\sпо\s(\D*)\sза\s(\S*)'  # TODO дописать
+        pattern = r'!удалить\sдз\sпо\s(\D*)\sза\s(\S*)'  # Шаблон для команды Удалить
         result = re.search(pattern, message)
         if not result:
             return {'error': 'invalid command format'}
         args = result.groups()
         subj = args[0]
-        for_date = args[1]
+        for_date_str = args[1]
+        for_date = convert_date(for_date_str)
         return {'command': 'удалить', 'subj': subj, 'for_date': for_date}
     else:
         return {'error': 'invalid command format'}
